ff7835f7b4441e991ec21b50432c0e8461b791d7
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-03-20 10:41:21 +0900

    directory-page-path

--- src/templates/directory_archive-template.js
+++ src/templates/directory_archive-template.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { graphql, navigate } from "gatsby"
-import { directoryArchivePath } from '../utils/archive_path'
 import ArchiveTemplate from './archive_template'
 
 export const query = graphql`
-    query($regex: String!, $skip: Int!, $limit: Int!){        
+    query($regex: String!, $directory: String!, $skip: Int!, $limit: Int!){        
       allMdx(sort:  {fields: frontmatter___date, order: DESC},
         filter: {
           frontmatter: { draft: {ne: true}},
@@ -14,26 +13,25 @@ export const query = graphql`
           ...postFields
         }
       }
+      aksDirectory(name: { eq: $directory }) {
+        id, name, label, fullLabel, pagePath
+      }
     }
   `
-const handleChange = (directory, p) => {
-  const path = directoryArchivePath(directory)
-  navigate((p === 1) ? path : `${path}/${p}`)
-}
 
+  const handleChange = (path, p) => {
+    navigate((p === 1) ? path : `${path}/${p}`)
+  }
+  
 export default function DirectoryArchiveTemplate({ data, pageContext }) {
-  const { directory } = pageContext
   const { breadcrumb: { crumbs } } = pageContext
-  //const current_directory = directory.split('/').slice(-1)
   const label = crumbs.slice(1).map(v => v.crumbLabel).join('/')
   const title = `DIRECTORY: ${label}`
-
-  //const picked = (({ foo, bar }) => ({ foo, bar }))(context);
 
   const pagination_parameters = {
     numberOfPages: pageContext.numberOfPages,
     humanPageNumber: pageContext.humanPageNumber,
-    onChangeHandler: (_e, p) => { handleChange(directory, p) }
+    onChangeHandler: (_e, p) => { handleChange(data.aksDirectory.pagePath, p) }
   }
   return (<ArchiveTemplate title={title} nodes={data.allMdx.nodes} crumbs={crumbs}
     pagination_parameters={pagination_parameters} />)
